fa7a1ab9437a0dea4398f7f3a5fecf600478b51e
Dustin Miller <dwmiller@anderson.edu>
2021-02-21 09:59:06 -0700

    Routing and form validation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,29 +85,17 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        /* <node>.json just for Firebase */
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Dustin',
-                address: {
-                    street: 'TestStreet 1',
-                    zipCode: '47362',
-                    country: 'USA'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) +
+                '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false})
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false})
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+          pathname: '/checkout',
+          search: '?' + queryString
+        });
     }
 
     render() {
